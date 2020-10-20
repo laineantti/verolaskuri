c2bae978a94d0a151e1b53d3a792df24d77b8f39
laineantti <laineantti@users.noreply.github.com>
2020-10-20 11:58:29 +0300

    testailua

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import React from 'react';
+import { useState } from 'react';
 //import './App.css';
-// lukumäärä???
+//lukumäärä???
 
-var laskuri=0;
+//type inference
+var laskuri="moi!";
 
 function App() {
-  
+  //array destructuring
+  const [laskuri,setLaskuri]=useState(0);
+
+  const hassuFunktio(x)=> {
+    x++;
+  }
+
   const nappiaPainettu = ()=>{
-    
-    laskuri++;
-    console.log("Painoit nappia")
+
+    setLaskuri(laskuri+1);
+    console.log(laskuri)
   }
   //JSX
   return (
     <div>
-        <button onClick={nappiaPainettu}>Paina minua</button>
+        <button onClick={nappiaPainettu}>Paina minua {laskuri}</button>
     </div>
   );
 }

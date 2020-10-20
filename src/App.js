103c9b657c476726692649e6e5e448e04999e719
laineantti <laineantti@users.noreply.github.com>
2020-10-20 15:20:35 +0300

    Viive korjattu

--- src/App.js
+++ src/App.js
@@ -1,34 +1,35 @@
-import React from 'react'
-import { useState } from 'react'
-
+import React from "react"
+import { useState } from "react"
 
 function App() {
-  //array destructuring 
+  //array destructuring
   const [bruttopalkka, setBruttopalkka] = useState(0)
   const [veroprosentti, setVeroprosentti] = useState(0)
-  const [veronmäärä, setVeronmäärä] = useState(0)
 
   const laskeVeronmaara = (event) => {
-    let vero = bruttopalkka * veroprosentti / 100
-    setVeronmäärä(vero)
+    return (bruttopalkka * veroprosentti) / 100
   }
 
   const palkkaMuuttunut = (event) => {
     setBruttopalkka(event.target.value)
-    laskeVeronmaara()
   }
 
   const veroprosMuuttunut = (event) => {
     setVeroprosentti(event.target.value)
-    laskeVeronmaara()
   }
 
   //JSX  //angular
   return (
     <div>
-      <input onChange={(event) => palkkaMuuttunut(event)} value={bruttopalkka}></input>
-      <input onChange={(event) => veroprosMuuttunut(event)} value={veroprosentti}></input>
-      <p>{veronmäärä}</p>
+      <input
+        onChange={(event) => palkkaMuuttunut(event)}
+        value={bruttopalkka}
+      ></input>
+      <input
+        onChange={(event) => veroprosMuuttunut(event)}
+        value={veroprosentti}
+      ></input>
+      <p>{laskeVeronmaara()}</p>
     </div>
   )
 }

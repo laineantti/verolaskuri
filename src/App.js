4d5dee61788343f21c8b10c8c115bc7bc46ee181
laineantti <laineantti@users.noreply.github.com>
2020-10-20 13:52:52 +0300

    tehtävänantoa varten päivitetty opettajan versioon

--- src/App.js
+++ src/App.js
@@ -1,28 +1,37 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState } from 'react'
 //import './App.css';
-//lukumäärä???
+// lukumäärä???
 
-//type inference
-var laskuri="moi!";
 
 function App() {
-  //array destructuring
-  const [laskuri,setLaskuri]=useState(0);
+  //array destructuring 
+  const [bruttopalkka, setBruttopalkka] = useState(0)
+  const [veroprosentti, setVeroprosentti] = useState(0)
+  const [veronmäärä, setVeronmäärä] = useState(0)
 
-  const hassuFunktio(x)=> {
-    x++;
+  const nappiaPainettu = () => {
+
+    let vero = bruttopalkka * veroprosentti / 100
+    setVeronmäärä(vero);
+  }
+  const palkkaMuuttunut = (event) => {
+    setBruttopalkka(event.target.value)
+    //   console.log("Teksti on muuttunut")
+  }
+  const veroprosMuuttunut = (event) => {
+    setVeroprosentti(event.target.value)
+    // setTeksti(event.target.value)
+    //     console.log("Teksti on muuttunut")
   }
 
-  const nappiaPainettu = ()=>{
-
-    setLaskuri(laskuri+1);
-    console.log(laskuri)
-  }
-  //JSX
+  //JSX  //angular
   return (
     <div>
-        <button onClick={nappiaPainettu}>Paina minua {laskuri}</button>
+      <button onClick={nappiaPainettu}>Laske veron määrä</button>
+      <input onChange={(event) => palkkaMuuttunut(event)} value={bruttopalkka}></input>
+      <input onChange={(event) => veroprosMuuttunut(event)} value={veroprosentti}></input>
+      <p>{veronmäärä}</p>
     </div>
   );
 }
